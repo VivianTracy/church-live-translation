438729dad0e3978008f3aa12f27b895749595965
zhenlian <zhenlian@mac.mynetworksettings.com>
2026-07-02 22:15:23 -0400

    put speechrecognitionconstructor in type

--- app/operator/page.tsx
+++ app/operator/page.tsx
@@ -10,23 +10,6 @@ import { MicrophoneCard } from "@/components/MicrophoneCard";
 import { AudienceCard } from "@/components/AudienceCard";
 import { AdvancedSettings } from "@/components/AdvancedSettings";
 
-type SpeechRecognitionConstructor = new () => {
-  lang: string;
-  continuous: boolean;
-  interimResults: boolean;
-  onstart: (() => void) | null;
-  onresult: ((event: any) => void) | null;
-  onerror: (() => void) | null;
-  onend: (() => void) | null;
-  start: () => void;
-};
-
-declare global {
-  interface Window {
-    SpeechRecognition?: SpeechRecognitionConstructor;
-    webkitSpeechRecognition?: SpeechRecognitionConstructor;
-  }
-}
 
 export default function OperatorPage() {
   // Operator session state
---
+++ types/speech-recognition.d.ts
@@ -0,0 +1,19 @@
+type SpeechRecognitionConstructor = new () => {
+  lang: string;
+  continuous: boolean;
+  interimResults: boolean;
+  onstart: (() => void) | null;
+  onresult: ((event: any) => void) | null;
+  onerror: (() => void) | null;
+  onend: (() => void) | null;
+  start: () => void;
+};
+
+declare global {
+  interface Window {
+    SpeechRecognition?: SpeechRecognitionConstructor;
+    webkitSpeechRecognition?: SpeechRecognitionConstructor;
+  }
+}
+
+export {};
